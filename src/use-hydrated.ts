f3a389a5984568f7d258c234a14c80dc3c189586
Sergio Xalambrí <hello@sergiodxa.com>
2021-07-20 14:24:56 -0500

    Create useHydrate hook

---
+++ src/use-hydrated.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+let hydrating = true;
+
+/**
+ * Return a boolean indicating if the JS has been hydrated already.
+ * When doing Server-Side Rendering, the result will always be false.
+ * When doing Client-Side Rendering, the result will always be false on the
+ * first render and true from then on. Even if a new component renders it will
+ * always start with true.
+ *
+ * Example: Disable a button that needs JS to work.
+ * ```tsx
+ * let hydrated = useHydrated();
+ * return (
+ *   <button type="button" disabled={!hydrated} onClick={doSomethingCustom}>
+ *     Click me
+ *   </button>
+ * );
+ * ```
+ */
+export function useHydrated() {
+  let [hydrated, setHydrated] = useState(() => !hydrating);
+
+  useEffect(function hydrate() {
+    hydrating = false;
+    setHydrated(true);
+  }, []);
+
+  return hydrated;
+}
