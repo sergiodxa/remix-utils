5257be45cb64dc80cd0a10fa9ac9a9e50bdb9b8b
Sergio Xalambrí <hello@sergiodxa.com>
2021-08-31 12:39:43 -0500

    Move types to another file

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,8 @@
-import {
-  ActionFunction,
-  HeadersFunction,
-  LinksFunction,
-  LoaderFunction,
-  MetaFunction,
-} from 'remix';
-
 export * from './client-only';
 export * from './csrf';
 export * from './outlet';
 export * from './parse-body';
 export * from './responses';
+export * from './types';
 export * from './use-hydrated';
 export * from './use-should-hydrate';
-
-export type LoaderArgs = Parameters<LoaderFunction>[0];
-export type ActionArgs = Parameters<ActionFunction>[0];
-export type LinksArgs = Parameters<LinksFunction>[0];
-export type MetaArgs = Parameters<MetaFunction>[0];
-export type HeadersArgs = Parameters<HeadersFunction>[0];
-
-export type LoaderReturn = ReturnType<LoaderFunction>;
-export type ActionReturn = ReturnType<ActionFunction>;
-export type LinksReturn = ReturnType<LinksFunction>;
-export type MetaReturn = ReturnType<MetaFunction>;
-export type HeadersReturn = ReturnType<HeadersFunction>;
