5867db517cb1f18d77085601b343c6f5b0d53de1
Sergio Xalambrí <hello@sergiodxa.com>
2021-11-03 19:58:32 -0500

    Create new bodyParser interface

--- src/server/parse-body.ts
+++ src/server/parse-body.ts
@@ -1,27 +1,34 @@
-/**
- * Parse the body of a Request to an URLSearchParams instance.
- * @example
- * async function action({ request }: ActionArgs): Promise<ActionReturn> {
- *  const body = await parseBody(request);
- *  await doSomething(body);
- *  return redirect("/target");
- * }
- */
-export function parseBody(request: Request): Promise<URLSearchParams> {
-  return request.text().then((body) => new URLSearchParams(body));
-}
+export let bodyParser = {
+  /**
+   * Parse the body of a Request to an URLSearchParams instance.
+   * @example
+   * async function action({ request }: ActionArgs): Promise<ActionReturn> {
+   *  const body = await bodyParser.toParams(request);
+   *  await doSomething(body);
+   *  return redirect("/target");
+   * }
+   */
+  async toParams(request: Request): Promise<URLSearchParams> {
+    const body = await request.text();
+    return new URLSearchParams(body);
+  },
 
-/**
- * Parse the params from parseBody result to an object.
- * @example
- * async function action({ request }: ActionArgs): Promise<ActionReturn> {
- *   const params = parseParams(await parseBody(request));
- *   await doSomething(params);
- *   return redirect("/target");
- * }
- */
-export function parseParams<Value extends Record<string, unknown>>(
-  params: URLSearchParams
-): Value {
-  return Object.fromEntries(params.entries()) as Value;
-}
+  /**
+   * Parse the body of a Request to a JSON object.
+   *
+   * The result is an unknown type since it's not possible to know the type of
+   * the object and the properties it will have after parsed from the body.
+   *
+   * You are encouraged to validate the result before using it.
+   * @example
+   * async function action({ request }: ActionArgs): Promise<ActionReturn> {
+   *   const params = await bodyParser.toJSON(request);
+   *   await doSomething(params);
+   *   return redirect("/target");
+   * }
+   */
+  async toJSON(request: Request): Promise<unknown> {
+    let params = await this.toParams(request);
+    return Object.fromEntries(params.entries()) as unknown;
+  },
+};
