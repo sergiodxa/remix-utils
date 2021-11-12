7358588f15952a733de792624ff7ecd3b9413961
Sergio Xalambrí <hello@sergiodxa.com>
2021-11-12 17:22:02 -0500

    Add tests for all Response helpers

--- test/server/responses.test.ts
+++ test/server/responses.test.ts
@@ -1,4 +1,15 @@
-import { notModified, redirectBack } from "../../src/server";
+import {
+  badRequest,
+  forbidden,
+  notFound,
+  notModified,
+  redirectBack,
+  serverError,
+  unauthorized,
+  unprocessableEntity,
+} from "../../src/server";
+
+let jsonContentType = "application/json; charset=utf-8";
 
 describe("Responses", () => {
   describe("redirectBack", () => {
@@ -31,6 +42,141 @@ describe("Responses", () => {
         new Response("", {
           status: 304,
           headers: { "X-Test": "it worked" },
+        })
+      );
+    });
+  });
+
+  describe("Bad Request", () => {
+    test("Should return Response with status 404", () => {
+      let response = badRequest({});
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 400,
+          headers: { "Content-Type": jsonContentType },
+        })
+      );
+    });
+
+    test("Should allow changing the Response headers", () => {
+      let response = badRequest({}, { headers: { "X-Test": "it worked" } });
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 400,
+          headers: { "X-Test": "it worked", "Content-Type": jsonContentType },
+        })
+      );
+    });
+  });
+
+  describe("Unauthorized", () => {
+    test("Should return Response with status 401", () => {
+      let response = unauthorized({});
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 401,
+          headers: { "Content-Type": jsonContentType },
+        })
+      );
+    });
+
+    test("Should allow changing the Response headers", () => {
+      let response = unauthorized({}, { headers: { "X-Test": "it worked" } });
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 401,
+          headers: { "X-Test": "it worked", "Content-Type": jsonContentType },
+        })
+      );
+    });
+  });
+
+  describe("Forbidden", () => {
+    test("Should return Response with status 403", () => {
+      let response = forbidden({});
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 403,
+          headers: { "Content-Type": jsonContentType },
+        })
+      );
+    });
+
+    test("Should allow changing the Response headers", () => {
+      let response = forbidden({}, { headers: { "X-Test": "it worked" } });
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 403,
+          headers: { "X-Test": "it worked", "Content-Type": jsonContentType },
+        })
+      );
+    });
+  });
+
+  describe("Not Found", () => {
+    test("Should return Response with status 404", () => {
+      let response = notFound({});
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 404,
+          headers: { "Content-Type": jsonContentType },
+        })
+      );
+    });
+
+    test("Should allow changing the Response headers", () => {
+      let response = notFound({}, { headers: { "X-Test": "it worked" } });
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 404,
+          headers: { "X-Test": "it worked", "Content-Type": jsonContentType },
+        })
+      );
+    });
+  });
+
+  describe("Unprocessable Entity", () => {
+    test("Should return Response with status 422", () => {
+      let response = unprocessableEntity({});
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 422,
+          headers: { "Content-Type": jsonContentType },
+        })
+      );
+    });
+
+    test("Should allow changing the Response headers", () => {
+      let response = unprocessableEntity(
+        {},
+        { headers: { "X-Test": "it worked" } }
+      );
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 422,
+          headers: { "X-Test": "it worked", "Content-Type": jsonContentType },
+        })
+      );
+    });
+  });
+
+  describe("Server Error", () => {
+    test("Should return Response with status 500", () => {
+      let response = serverError({});
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 500,
+          headers: { "Content-Type": jsonContentType },
+        })
+      );
+    });
+
+    test("Should allow changing the Response headers", () => {
+      let response = serverError({}, { headers: { "X-Test": "it worked" } });
+      expect(response).toEqual(
+        new Response("{}", {
+          status: 500,
+          headers: { "X-Test": "it worked", "Content-Type": jsonContentType },
         })
       );
     });
