5f2dca656e9335de71715e4288f32c33e3110f31
Sergio Xalambrí <hello@sergiodxa.com>
2022-03-21 14:36:22 -0500

    Add isPrefetch helper (#60)

---
+++ test/server/is-prefetch.test.ts
@@ -0,0 +1,21 @@
+import { isPrefetch } from "../../src";
+
+describe(isPrefetch, () => {
+  test.each([
+    "Purpose",
+    "X-Purpose",
+    "Sec-Purpose",
+    "Sec-Fetch-Purpose",
+    "Moz-Purpose",
+  ])("should return true when %s is set to prefetch", (header) => {
+    let request = new Request("https://example.com/", {
+      headers: { [header]: "prefetch" },
+    });
+    expect(isPrefetch(request)).toBe(true);
+  });
+
+  test("should return false if it's not a prefetch", () => {
+    let request = new Request("https://example.com/");
+    expect(isPrefetch(request)).toBe(false);
+  });
+});
