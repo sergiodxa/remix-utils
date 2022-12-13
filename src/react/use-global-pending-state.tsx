a903b517ddaca1c140e7fe56b017350ed39ad6e4
Sergio Xalambrí <hello@sergiodxa.com>
2022-12-12 23:40:28 -0500

    Add useGlobalTransitionStates, useGlobalLoadingState and useGlobalSubmittingState hooks (#129)

--- src/react/use-global-pending-state.tsx
+++ src/react/use-global-pending-state.tsx
@@ -2,29 +2,65 @@ import { useTransition, useFetchers } from "@remix-run/react";
 import { useMemo } from "react";
 
 /**
- * Let you know if the app is pending some request, either global transition
- * or some fetcher transition.
- * @returns "idle" | "pending"
+ * This is a helper hook that returns the state of every fetcher active on
+ * the app and combine it with the state of the global transition.
+ * @example
+ * let states = useGlobalTransitionStates();
+ * if (state.includes("loading")) {
+ *   // The app is loading.
+ * }
+ * if (state.includes("submitting")) {
+ *   // The app is submitting.
+ * }
+ * // The app is idle
  */
-export function useGlobalPendingState() {
+export function useGlobalTransitionStates() {
   let transition = useTransition();
   let fetchers = useFetchers();
 
   /**
    * This gets the state of every fetcher active on the app and combine it with
-   * the state of the global transition (Link and Form), then use them to
-   * determine if the app is idle or if it's loading.
-   * Here we consider both loading and submitting as loading.
+   * the state of the global transition (Link and Form).
    */
-  return useMemo<"idle" | "pending">(
-    function getGlobalState() {
-      let states = [
-        transition.state,
-        ...fetchers.map((fetcher) => fetcher.state),
-      ];
-      if (states.every((state) => state === "idle")) return "idle";
-      return "pending";
+  return useMemo(
+    function getGlobalTransitionStates() {
+      return [transition.state, ...fetchers.map((fetcher) => fetcher.state)];
     },
     [transition.state, fetchers]
   );
+}
+
+/**
+ * Let you know if the app is pending some request, either global transition
+ * or some fetcher transition.
+ * @returns "idle" | "pending"
+ */
+export function useGlobalPendingState() {
+  let isSubmitting = useGlobalSubmittingState() === "submitting";
+  let isLoading = useGlobalLoadingState() === "loading";
+
+  if (isLoading || isSubmitting) return "pending";
+  return "idle";
+}
+
+/**
+ * Let you know if the app is submitting some request, either global transition
+ * or some fetcher transition.
+ * @returns "idle" | "submitting"
+ */
+export function useGlobalSubmittingState() {
+  let states = useGlobalTransitionStates();
+  if (states.includes("submitting")) return "submitting";
+  return "idle";
+}
+
+/**
+ * Let you know if the app is loading some request, either global transition
+ * or some fetcher transition.
+ * @returns "idle" | "loading"
+ */
+export function useGlobalLoadingState() {
+  let states = useGlobalTransitionStates();
+  if (states.includes("loading")) return "loading";
+  return "idle";
 }
