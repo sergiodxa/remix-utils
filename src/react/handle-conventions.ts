919fbc653190b050c4f3d6ad577c27f5555adbdb
Leo Singer <leo.singer@ligo.org>
2023-04-13 12:09:12 -0400

    Fix types for RouteData
    Since remix-run/remix#5679 and remix v1.15.0, RouteData is no
    longer exported by Remix. The type is now provided by
    `@remix-run/router`.

--- src/react/handle-conventions.ts
+++ src/react/handle-conventions.ts
@@ -1,7 +1,9 @@
-import { RouteData } from "@remix-run/react/dist/routeData";
+import type { RouterState } from "@remix-run/router";
 import type { AppData } from "@remix-run/server-runtime";
 import type { Location, Params } from "react-router-dom";
 import { Matches } from "./matches-type";
+
+type RouteData = RouterState["loaderData"];
 
 export type HandleConventionArguments<Data extends AppData = AppData> = {
   id: string;
