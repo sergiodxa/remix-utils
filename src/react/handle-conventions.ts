9af1f5b1f0b1f5e1bcf2c1bdf1ee88005f3c7495
Dan Reeves <hey@danreev.es>
2023-04-16 01:23:34 +0100

    Get RouteData type from remix-run/router rather than remix-run/react internals

--- src/react/handle-conventions.ts
+++ src/react/handle-conventions.ts
@@ -1,11 +1,12 @@
-import { RouteData } from "@remix-run/react/dist/routeData";
 import type { AppData } from "@remix-run/server-runtime";
 import type { Location, Params } from "react-router-dom";
+import type { RouterState } from "@remix-run/router";
+type RouteData = RouterState["loaderData"];
 
 export type HandleConventionArguments<Data extends AppData = AppData> = {
-  id: string;
-  data: Data;
-  params: Params;
-  location: Location;
-  parentsData: RouteData;
+	id: string;
+	data: Data;
+	params: Params;
+	location: Location;
+	parentsData: RouteData;
 };
