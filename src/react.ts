d39680b1023064ed35fca6164d4acc9623e2b523
Sergio Xalambrí <hello@sergiodxa.com>
2023-06-12 19:20:08 -0500

    Export ServerOnly component (#209)

--- src/react.ts
+++ src/react.ts
@@ -4,6 +4,7 @@ export * from "./react/csrf";
 export * from "./react/dynamic-links";
 export * from "./react/external-scripts";
 export * from "./react/matches-type";
+export * from "./react/server-only";
 export * from "./react/structured-data";
 export * from "./react/use-event-source";
 export * from "./react/use-global-pending-state";
