aaf148570766d563b3669948e1559c79ce93ebc2
Sergio Xalambrí <hello@sergiodxa.com>
2023-07-22 02:34:11 -0500

    Remove deprecated DynamicLinks

--- src/react.ts
+++ src/react.ts
@@ -1,7 +1,6 @@
 export * from "./react/cache-assets";
 export * from "./react/client-only";
 export * from "./react/csrf";
-export * from "./react/dynamic-links";
 export * from "./react/external-scripts";
 export * from "./react/matches-type";
 export * from "./react/server-only";
