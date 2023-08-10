1c0fe973c5ea326683b8f3c38483277d3aadf78f
Sergio Xalambrí <hello@sergiodxa.com>
2023-08-10 16:48:32 -0500

    Update handle-conventions.ts

--- src/react/handle-conventions.ts
+++ src/react/handle-conventions.ts
@@ -1,5 +1,6 @@
-import type { RouterState, Location, Params } from "@remix-run/router";
+import type { RouterState } from "@remix-run/router";
 import type { AppData } from "@remix-run/server-runtime";
+import type { Location, Params } from "@remix-run/react";
 import { Matches } from "./matches-type";
 
 export type HandleConventionArguments<Data extends AppData = AppData> = {
