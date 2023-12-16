23523d101d1407c346223884f8394685750fd2d7
Jacob Paris <jacob@jacobparis.com>
2023-12-16 23:24:37 +0800

    feature: add use-debounce-submit

---
+++ src/react/use-debounce-submit.ts
@@ -0,0 +1,55 @@
+import type { SubmitOptions, SubmitFunction } from "@remix-run/react";
+import { useSubmit } from "@remix-run/react";
+import { useCallback, useEffect, useRef } from "react";
+
+type SubmitTarget = Parameters<SubmitFunction>["0"];
+
+export function useDebounceSubmit() {
+	let timeoutRef = useRef<NodeJS.Timeout | undefined>();
+
+	useEffect(() => {
+		// no initialize step required since timeoutRef defaults undefined
+		let timeout = timeoutRef.current;
+		return () => {
+			if (timeout) clearTimeout(timeout);
+		};
+	}, [timeoutRef]);
+
+	// Clone the original submit to avoid a recursive loop
+	const originalSubmit = useSubmit();
+
+	const submit = useCallback(
+		(
+			/**
+			 * Specifies the `<form>` to be submitted to the server, a specific
+			 * `<button>` or `<input type="submit">` to use to submit the form, or some
+			 * arbitrary data to submit.
+			 *
+			 * Note: When using a `<button>` its `name` and `value` will also be
+			 * included in the form data that is submitted.
+			 */
+			target: SubmitTarget,
+			/**
+			 * Options that override the `<form>`'s own attributes. Required when
+			 * submitting arbitrary data without a backing `<form>`. Additionally, you
+			 * can specify a `debounceTimeout` to delay the submission of the data.
+			 */
+			options?: SubmitOptions & {
+				/** Submissions within this timeout will be canceled */
+				debounceTimeout?: number;
+			},
+		) => {
+			if (timeoutRef.current) clearTimeout(timeoutRef.current);
+			if (!options?.debounceTimeout || options.debounceTimeout <= 0) {
+				return originalSubmit(target, options);
+			}
+
+			timeoutRef.current = setTimeout(() => {
+				originalSubmit(target, options);
+			}, options.debounceTimeout);
+		},
+		[originalSubmit],
+	);
+
+	return submit;
+}
