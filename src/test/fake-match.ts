ee5dfb300aa957fca6417b1b979ca76d51d690ce
Sergio Xalambrí <hello@sergiodxa.com>
2024-12-04 01:56:45 -0500

    Remove unused fakeMatch function and its associated file

--- src/test/fake-match.ts
+++
@@ -1,4 +0,0 @@
-// biome-ignore lint/suspicious/noExplicitAny: RouteData is not exported anymore, route match has issues
-export function fakeMatch(data?: any): any {
-	return { id: "root", data, pathname: "/", handle: {}, params: {} };
-}
